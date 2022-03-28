ee5bcdbab177730c1b42e7981aa0c4593727e54d
Sanyaur <gs.ujvari@gmail.com>
2022-03-28 17:38:35 +0200

    useState import removed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
